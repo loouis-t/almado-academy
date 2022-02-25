a16ef7cd852f411e789bdf07bd270c02e77ef1a2
Louis Travaux <louis.trvx@gmail.com>
2022-02-25 21:42:14 +0100

    début page accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'almado-academy';
+  current_year = new Date().getFullYear();
+
+  displayMenu() {
+    let checkbox = <HTMLInputElement>document.getElementById("check");
+    let menu = document.getElementById("menu");
+    let navBar = document.getElementById("navBar");
+
+    if (checkbox.checked) {
+      menu!.classList.add("menuOuvert");
+      navBar!.classList.add("navBar");
+    } else {
+      menu!.classList.remove("menuOuvert");
+      navBar!.classList.remove("navBar");
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ErrPageComponent } from './err-page/err-page.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'contact', component: ContactComponent },
+      { path: '', component: HomeComponent },
+      { path: '404', component: ErrPageComponent},
+      { path: '**', redirectTo: '/404'}
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HomeComponent,
+    ErrPageComponent
+  ]
 })
 export class AppModule { }
